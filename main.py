be45aa6c534b3e8e60b00628633f96f409000506
Shreya Pal <palshreya001@gmail.com>
2023-11-17 14:49:10 +0530

    commiting and pushing changes to the remote repository

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.logging import logger
 
 
 STAGE_NAME = "Data Ingestion stage"
-
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
@@ -16,7 +16,6 @@ except Exception as e:
 
 
 STAGE_NAME = "Data Validation stage"
-
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_validation = DataValidationTrainingPipeline()
@@ -25,3 +24,4 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
