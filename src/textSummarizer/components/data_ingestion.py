8069b8b473e52f3821ddd82b628c0f1e0991f19e
Shreya Pal <palshreya001@gmail.com>
2023-11-20 18:31:32 +0530

    prediction and UI added

--- src/textSummarizer/components/data_ingestion.py
+++ src/textSummarizer/components/data_ingestion.py
@@ -12,12 +12,14 @@ class DataIngestion:
         self.config = config
 
     def download_file(self):
+        request.urlcleanup()
         if not os.path.exists(self.config.local_data_file):
             filename, headers = request.urlretrieve(
                 url = self.config.source_URL,
                 filename = self.config.local_data_file
             )
             logger.info(f"{filename} download! with following info: \n{headers}")
+            request.urlcleanup()
 
         else:
             logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")
